01c9c4a61da3e7d2be7d880794ebefdc22957543
Bartłomiej Mumot <mumot1999@gmail.com>
2020-11-30 15:26:42 +0100

    init

---
+++ sk2/lab8/epollServer.c
@@ -0,0 +1,109 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <pthread.h>
+
+#define SERVER_PORT 1234
+#define QUEUE_SIZE 5
+
+//struktura zawierająca dane, które zostaną przekazane do wątku
+struct thread_data_t
+{
+//TODO
+};
+
+//funkcja opisującą zachowanie wątku - musi przyjmować argument typu (void *) i zwracać (void *)
+void *ThreadBehavior(void *t_data)
+{
+    pthread_detach(pthread_self());
+    struct thread_data_t *th_data = (struct thread_data_t*)t_data;
+
+    //TODO
+    for(;;) {
+        epoll_wait(TODO, TODO, 1, -1); //1 = maksymalnie jedno zdarzenie, -1 = bez timeout
+        if ( TODO & EPOLLIN ) {
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+
+int main(int argc, char* argv[])
+{
+   int server_socket_descriptor;
+   int connection_socket_descriptor;
+   int bind_result;
+   int listen_result;
+   char reuse_addr_val = 1;
+   struct sockaddr_in server_address;
+    int efd;
+
+   //inicjalizacja gniazda serwera
+   
+   memset(&server_address, 0, sizeof(struct sockaddr));
+   server_address.sin_family = AF_INET;
+   server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+   server_address.sin_port = htons(SERVER_PORT);
+
+   server_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+   if (server_socket_descriptor < 0)
+   {
+       fprintf(stderr, "%s: Błąd przy próbie utworzenia gniazda..\n", argv[0]);
+       exit(1);
+   }
+   setsockopt(server_socket_descriptor, SOL_SOCKET, SO_REUSEADDR, (char*)&reuse_addr_val, sizeof(reuse_addr_val));
+
+   bind_result = bind(server_socket_descriptor, (struct sockaddr*)&server_address, sizeof(struct sockaddr));
+   if (bind_result < 0)
+   {
+       fprintf(stderr, "%s: Błąd przy próbie dowiązania adresu IP i numeru portu do gniazda.\n", argv[0]);
+       exit(1);
+   }
+
+   listen_result = listen(server_socket_descriptor, QUEUE_SIZE);
+   if (listen_result < 0) {
+       fprintf(stderr, "%s: Błąd przy próbie ustawienia wielkości kolejki.\n", argv[0]);
+       exit(1);
+   }
+
+   //TODO stworzyć instancję epoll przez epoll_create1(0);
+    efd = epoll_create1 (0);
+    if (efd == -1)
+    {
+        perror ("epoll_create");
+        abort ();
+    }
+
+   int create_result = 0;
+   pthread_t thread1;
+   create_result = pthread_create(&thread1, NULL, ThreadBehavior, TODO);
+   if (create_result){
+      printf("Błąd przy próbie utworzenia wątku, kod błędu: %d\n", create_result);
+      exit(-1);
+   }
+
+   while(1)
+   {
+       connection_socket_descriptor = accept(server_socket_descriptor, NULL, NULL);
+       if (connection_socket_descriptor < 0)
+       {
+           fprintf(stderr, "%s: Błąd przy próbie utworzenia gniazda dla połączenia.\n", argv[0]);
+           exit(1);
+       }
+
+	//TODO dodać deskryptor połączenia do zbioru obserwowanego przez epoll
+   }
+
+   close(server_socket_descriptor);
+   return(0);
+}
